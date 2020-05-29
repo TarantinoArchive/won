39b39a3fa16aa7c082a8bbf2dc5891e6769cdce5
--global <--global>
2020-05-29 23:11:23 +0200

    Add test script

--- .github/test/test.sh
+++ .github/test/test.sh
@@ -1,4 +1,6 @@
+cd ./.github/test
 won -hj testHtml.html -o test.json
 cmp --silent testJSON.json test.json && echo 'HTML to JSON: Success' || echo 'HTML to JSON: Failed'
 won -cj testCSS.css -o test.json
 cmp --silent test.json testJss.json && echo 'CSS to JSON: Success' || echo 'CSS to JSON: Failed'
+cd ../..
